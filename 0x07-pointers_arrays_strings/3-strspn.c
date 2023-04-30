32a32274dfcc64b10f170a0b7317fa2374f7afbd
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2023-04-30 07:14:45 -0700

    Task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/*
+ * unsigned int _strspn(char *s, char *accept) - Returns the number of bytes in the initial segment of s which consist only of bytes from accept
+ *
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int n = 0;
+	int r = 0;
+	while(*s)
+	{
+		for(r = 0; accept[r]; r++)
+		{
+			if(accept[r]==*s)
+			{
+				n++;
+				break;
+			}
+			else if(accept[r+1] == '\0')
+			{
+				return(n);
+			}
+		}
+		s++;
+	}
+}	
