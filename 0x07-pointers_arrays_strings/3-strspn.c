5401c70edebc05dfd4a80aeed1aacabd4e87a9ac
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2023-04-30 07:22:20 -0700

    Task 3 correction 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,20 +9,21 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int n = 0;
 	int r;
-	while(*s)
+
+	while (*s)
 	{
-		for(r = 0; accept[r]; r++)
+		for (r = 0; accept[r]; r++)
 		{
-			if(accept[r]==*s)
+			if (*s == accept[r])
 			{
 				n++;
 				break;
 			}
-			else if(accept[r+1] == '\0')
-			{
-				return(n);
-			}
+			else if (accept[r + 1] == '\0')
+				return (n);
 		}
 		s++;
 	}
-}	
+	return (n);
+}
+	
