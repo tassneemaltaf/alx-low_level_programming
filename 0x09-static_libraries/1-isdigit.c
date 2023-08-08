e221716ddcd47ba653f96a5939bd85e77aae99c8
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2023-08-08 13:07:02 -0700

    Resst of task 0 added

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/*
+ * _isdigit - confirms if it is a digit
+ * @c: int input
+ * Returns: 1 if int, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	return (c >= 0 && c <= 9);
+}
---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _islower - checks if character is lowecase
+ * @c: character input
+ * Returns: 1 if true, 0 otherwise
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
