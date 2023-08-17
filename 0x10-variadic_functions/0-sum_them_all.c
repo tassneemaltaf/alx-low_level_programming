184333d9a172c71c2abcd98730a02125c4a506cb
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2023-08-17 13:30:27 -0700

    alterations made

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,11 +8,11 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	if (n == 0)
-		return (0);
-
 	va_list ap;
 	unsigned int i, sum = 0;
+
+	if (n == 0)
+		return(0);
 
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
