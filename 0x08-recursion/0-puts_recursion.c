bdb18cb42a9c23b507087ce65abc59656cf37de7
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2023-08-30 13:42:48 -0700

    Task o recursion added

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string followed by a new line
+ * @s: string
+ * Return: 0 if successful
+ */
+void _puts_recursion(char *s)
+{
+	if(*s == '\0')
+	{
+		_putchar(10);
+	}
+	else
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+This folder contains the tasks for the recursion topic
