25ab1ac5bf1fb9d7aedfb3e701be94c5be759474
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2023-09-03 04:09:57 -0700

    Task 0 added

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints the program name
+ * @argc: number of command line arguments
+ * @argv: array that contains the programs command line arguments
+ * Return: 0 (success)
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n",argv[0]);
+	return (0);
+}
