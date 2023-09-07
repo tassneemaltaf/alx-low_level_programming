09225bdedecf38554885563be830fe9bd25fa44d
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2023-09-07 13:07:29 -0700

    Checking

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,6 +9,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *ar;
+	int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -17,6 +18,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (ar == NULL)
 		return (NULL);
+
+	for (i = 0; i < (nmemb * size); i++)
+		ar[i] = 0;
 
 	return (ar);
 }
