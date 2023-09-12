825d5f6c03282e71eb1435eaef4590aca7509ac9
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2023-09-12 09:58:10 -0700

    dog.h file added

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+typedef struct dog dog_t;
+
+/**
+ * struct dog - contains dog info
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
