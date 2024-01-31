cf3176c9fe77329046eb91f2fcbe8d68906a8b2a
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2024-01-31 03:07:12 -0800

    Betty changes

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include "main.h"
-/*
+/**
  * _puts_recursion - prints a string, followed by a new line.
- * @s: string input
- * Returns: nothing
+ * @s: string
+ * Return: no return.
  */
 void _puts_recursion(char *s)
 {
