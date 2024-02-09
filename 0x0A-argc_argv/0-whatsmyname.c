9931e9235630bdbb94c623d3d88fabfe40674dee
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2024-02-09 12:31:37 -0800

    Tasks added

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints its name, followed by a new line.
+ * @argc: number of command line args
+ * @argv: command line args
+ * Return: returns 0 if success.
+ */
+int main(int argc __attribute__((unused)), char* argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
