fd804002e044628414e49b8ff273c1299346ba3e
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2024-02-15 01:46:59 -0800

    changes made to file 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - prints the name of the file it was compiled from, followed by a new line.
+ * main - prints the name of the file it was compiled from
  * Return: 0 if success
  */
 int main(void)
