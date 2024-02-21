06a1933b133f53a36a6019c4d4ecd308142c83db
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2024-02-20 20:26:11 -0800

    More details added to file dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,17 @@
-typedef struct dog
-{
-        char *name;
-        float age;
-        char *owner;
-}dog_t;
-
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - a struct that contains dogs infos.
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
