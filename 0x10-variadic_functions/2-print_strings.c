9cad3961e43d8b26c6f276b01fdfdffa0f1fb2b6
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2024-02-26 02:24:26 -0800

    Mods added to file 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,7 +19,14 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		str = va_arg(my_strings, char *);
 
-		printf("%s ", str);
+		if (str == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			printf("%s ", str);
+		}
 
 		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
