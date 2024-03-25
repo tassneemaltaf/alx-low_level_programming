c568e874e74e6ecf08f9c6354de431ad0554502c
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2024-03-25 13:29:07 -0700

    Task 0 edited

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include "lists.h"
+/**
+ * print_dlistint - prints size of doubly linked list
+ * @h: head node
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t n;
+
 	n = 0;
 	while (h != NULL)
 	{
@@ -10,5 +15,5 @@ size_t print_dlistint(const dlistint_t *h)
 		h = (*h).next;
 		++n;
 	}
-	return n;
+	return (n);
 }
