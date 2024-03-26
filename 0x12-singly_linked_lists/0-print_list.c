26d75476ec41a31aa2f7cd7af1d1f252f79d0cb4
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2024-03-25 20:36:33 -0700

    First task added

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list - prints a singly linked list
+ * @h: head node
+ */
+size_t print_list(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+			h = h->next;
+		}
+		++n;
+	}
+	return (n);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,2 @@
+In this folder we have tasks related to singly linked lists in c
+
